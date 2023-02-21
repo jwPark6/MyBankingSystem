08184f61a6ad9c2fcf8f4b58c6efb9f2225a7e99
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-02-21 23:28:38 +0900

    feat : Customer 중복 아이디, 이메일 검증 메소드 추가

--- src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
+++ src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
@@ -4,4 +4,6 @@ import bankingSystem.server.domain.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUserId(String userId);
 }
