16756c6a3f168118cb295bb59add23b4fed46495
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-02-21 22:29:09 +0900

    feat : customer 레포지토리 추가

---
+++ src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package bankingSystem.server.domain.customer.repository;
+
+import bankingSystem.server.domain.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
