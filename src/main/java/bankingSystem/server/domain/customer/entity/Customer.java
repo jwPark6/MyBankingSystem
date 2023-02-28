a28ea990b1d8718eaa675f07864a0cb53ceb5b0d
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-02-28 21:35:45 +0900

    refactor : PK 외 FK 추가 위해 serializable 상속 추가

--- src/main/java/bankingSystem/server/domain/customer/entity/Customer.java
+++ src/main/java/bankingSystem/server/domain/customer/entity/Customer.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Customer {
+public class Customer implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
