5eb10301e18d0c7b05bf83cb9bfc566fc81bc77d
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-02-28 20:46:20 +0900

    feat : 계좌 입출금 기능 추가

--- src/main/java/bankingSystem/server/domain/account/service/AccountService.java
+++ src/main/java/bankingSystem/server/domain/account/service/AccountService.java
@@ -3,17 +3,39 @@ package bankingSystem.server.domain.account.service;
 import bankingSystem.server.domain.account.entity.Account;
 import bankingSystem.server.domain.account.repository.AccountRepository;
 import bankingSystem.server.domain.customer.entity.Customer;
+import bankingSystem.server.domain.customer.repository.CustomerRepository;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
+    @Autowired
     private final AccountRepository accountRepository;
+
+    @Autowired
+    private final CustomerRepository customerRepository;
 
     public void register(Customer customer) {
         Account account = new Account(customer, 0);
         accountRepository.save(account);
+    }
+
+    @Transactional
+    public int deposit(String userId, int money) {
+        Customer customer = customerRepository.findByUserId(userId);
+        Account account = accountRepository.findByCustomerId(customer.getId());
+        return account.updateBalance(money);
+    }
+
+    @Transactional
+    public int withdraw(String userId, int money) {
+        Customer customer = customerRepository.findByUserId(userId);
+        Account account = accountRepository.findByCustomerId(customer.getId());
+        return account.updateBalance(money * -1);
     }
 }
