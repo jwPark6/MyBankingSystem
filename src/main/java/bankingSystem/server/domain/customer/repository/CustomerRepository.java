ca22f176d393e4c5ec33ce2ccb02acd3f926a043
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-02-28 18:09:17 +0900

    feat : 단순 로그인(세션 미구현)

--- src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
+++ src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
@@ -3,7 +3,11 @@ package bankingSystem.server.domain.customer.repository;
 import bankingSystem.server.domain.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmail(String email);
     boolean existsByUserId(String userId);
+
+    Customer findByUserId(String userId);
 }
