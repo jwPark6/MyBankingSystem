271d21bdd29b721c0c4d7f0c2ddd11f6d6cd1e9f
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-01 16:37:47 +0900

    feat : 친구 삭제 기능 추가

--- src/main/java/bankingSystem/server/domain/friend/repository/FriendRepository.java
+++ src/main/java/bankingSystem/server/domain/friend/repository/FriendRepository.java
@@ -1,11 +1,21 @@
 package bankingSystem.server.domain.friend.repository;
 
+import bankingSystem.server.domain.customer.entity.Customer;
 import bankingSystem.server.domain.friend.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
-    Friend findByCustomerUserId(String customerUserId);
+    List<Friend> findByCustomerUserId(String customerUserId);
 
     Friend findByCustomerUserIdAndFriendUserId(String customerUserId, String friendUserId);
+
+    @Modifying
+    @Query("delete from Friend f where f.customer = :customer")
+    void removeAllByCustomerUserId(@Param("customer") Customer customer);
 }
