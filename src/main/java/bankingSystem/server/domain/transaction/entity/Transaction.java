1ba82b633a42f75e7bb2b467111b0c984aac7caa
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-01 22:08:38 +0900

    refactor : 연관관계 수정

--- src/main/java/bankingSystem/server/domain/transaction/entity/Transaction.java
+++ src/main/java/bankingSystem/server/domain/transaction/entity/Transaction.java
@@ -1,20 +1,24 @@
 package bankingSystem.server.domain.transaction.entity;
 
 import bankingSystem.server.domain.account.entity.Account;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
     private Account account;
@@ -27,4 +31,12 @@ public class Transaction {
 
     @NotNull
     private int amtWithdrawal;
+
+    public Transaction(Account account, Long foreignAccountId, LocalDateTime timeStamp, int amtWithdrawal) {
+        this.account = account;
+        this.foreignAccountId = foreignAccountId;
+        this.timeStamp = timeStamp;
+        this.amtWithdrawal = amtWithdrawal;
+        account.getTransactions().add(this);
+    }
 }
