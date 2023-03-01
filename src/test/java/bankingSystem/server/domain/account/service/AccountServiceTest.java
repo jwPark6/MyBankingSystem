2bfa78c48ae27ebe335fe9020ef81a3a9b0ccc60
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-01 22:11:48 +0900

    test : fetch join 계좌 조회 테스트

--- src/test/java/bankingSystem/server/domain/account/service/AccountServiceTest.java
+++ src/test/java/bankingSystem/server/domain/account/service/AccountServiceTest.java
@@ -5,10 +5,13 @@ import bankingSystem.server.domain.account.repository.AccountRepository;
 import bankingSystem.server.domain.customer.entity.Customer;
 import bankingSystem.server.domain.customer.repository.CustomerRepository;
 import bankingSystem.server.domain.customer.service.CustomerService;
+import bankingSystem.server.domain.transaction.entity.Transaction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@RequiredArgsConstructor
+@Slf4j
 class AccountServiceTest {
 
     @Autowired
@@ -43,4 +46,16 @@ class AccountServiceTest {
         assertThat(account.getBalance()).isEqualTo(1000);
     }
 
+    @Test
+    public void 계좌조회() {
+        Account account = accountService.findAllWithTransaction("asd123");
+        List<Transaction> txs = account.getTransactions();
+
+        log.info("account = {}", account);
+
+        for (Transaction tx : txs) {
+            log.info("account tx = {}", tx.getAmtWithdrawal());
+        }
+
+    }
 }
