bb8f86fd596753bdcc6bee3509abd8ba90ff5ede
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-02 02:14:07 +0900

    refactor : Friend Entity DTO로 변경

--- src/main/java/bankingSystem/server/domain/customer/dto/CustomerWithFriendDto.java
+++ src/main/java/bankingSystem/server/domain/customer/dto/CustomerWithFriendDto.java
@@ -1,10 +1,12 @@
 package bankingSystem.server.domain.customer.dto;
 
 import bankingSystem.server.domain.customer.entity.Customer;
+import bankingSystem.server.domain.friend.dto.FriendDto;
 import bankingSystem.server.domain.friend.entity.Friend;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class CustomerWithFriendDto {
@@ -13,12 +15,15 @@ public class CustomerWithFriendDto {
     private String email;
     private String userId;
 
-    private List<Friend> friends;
+    private List<FriendDto> friends;
 
     public CustomerWithFriendDto(Customer customer) {
         this.name = customer.getName();
         this.email = customer.getEmail();
         this.userId = customer.getUserId();
-        this.friends = customer.getFriends();
+        List<Friend> fs = customer.getFriends();
+        this.friends = fs.stream()
+                .map(o -> new FriendDto(o.getFriendUserId()))
+                .collect(Collectors.toList());
     }
 }
