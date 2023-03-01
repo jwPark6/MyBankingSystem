d66792e206125b4dae3a3af280637e4b320929ec
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-02 01:06:45 +0900

    feat : 친구 목록 조회 구현

--- src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
+++ src/main/java/bankingSystem/server/domain/customer/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package bankingSystem.server.domain.customer.repository;
 
 import bankingSystem.server.domain.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +11,8 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
     boolean existsByEmail(String email);
     boolean existsByUserId(String userId);
 
+    @Query("select c from Customer c" +
+            " join fetch c.friends f" +
+            " where c.userId = :userId")
+    Customer findByUserIdWithFriends(@Param("userId") String userId);
 }
