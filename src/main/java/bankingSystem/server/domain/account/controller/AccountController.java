856132d12abb34d91496e2954ed68351c050fd8d
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-03-02 21:45:50 +0900

    feat : account api 구현

--- src/main/java/bankingSystem/server/domain/account/controller/AccountController.java
+++ src/main/java/bankingSystem/server/domain/account/controller/AccountController.java
@@ -6,6 +6,7 @@ import bankingSystem.server.domain.account.repository.AccountRepository;
 import bankingSystem.server.domain.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountService accountService;
-
     @GetMapping("/account")
     public AccountDto account (@RequestParam("userId") String userId) {
         return accountService.findAllWithTransaction(userId);
+    }
+
+    @PostMapping("/account/deposit")
+    public int deposit(@RequestParam("money") int money,
+                       @RequestParam("userId") String userId) {
+        return accountService.deposit(userId, money);
+    }
+
+    @PostMapping("/account/lockDeposit")
+    public int lockDeposit(@RequestParam("money") int money,
+                       @RequestParam("userId") String userId) throws InterruptedException {
+        return accountService.lockDeposit(userId, money);
+    }
+
+    @PostMapping("/account/withdraw")
+    public int withdraw(@RequestParam("money") int money,
+                        @RequestParam("userId") String userId) {
+        return accountService.withdraw(userId, money);
+    }
+
+    @PostMapping("/account/transfer")
+    public int transfer(@RequestParam("money") int money,
+                        @RequestParam("userId") String userId,
+                        @RequestParam("fUserId") String fUserId) {
+        return accountService.transfer(userId, money, fUserId);
     }
 }
